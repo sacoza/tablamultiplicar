1e29fbde672663e8f643a66db37ddb61fe0d1b84
Sandro Comes Zacares <sacoza@floridauniversitaria.es>
2017-10-19 09:20:07 +0200

    first commit

--- tabla.js
+++ tabla.js
@@ -1,14 +1,14 @@
 var i=0;
-var numero=prompt('La tabla de multiplicar del:');
+var numero=parseInt(prompt('La tabla de multiplicar del:'));
 document.write("<table border= '1'<tr><td>Número</td><td>Resultado</td></tr>")
 while(numero>100){
-  alert("error");
+  alert("ERROR");
   var numero=prompt('La tabla de multiplicar del:');
 }
 
 if(numero<=100){
 for(i=0; i<=10; i++) {
-document.write("<tr><td>"+numero+" X " + i + "</td><td>" + numero*i  + "</td></tr>");
+document.write("<tr><td>"+ numero +" X " + i + "</td><td>" + numero * i  + "</td></tr>");
 }
 
 document.write("</table>");
